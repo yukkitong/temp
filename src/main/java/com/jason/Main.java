132302649820ca164fc2e65cf029740b1b7a0bb2
yukkitong <jason98.bae@gmail.com>
2018-10-28 21:44:24 +0900

    update

--- src/main/java/com/jason/Main.java
+++ src/main/java/com/jason/Main.java
@@ -29,14 +29,13 @@ public class Main {
         return calendar.getTime();
     }
 
-    private static final String END_POINT = "http://api.visitkorea.or.kr/openapi/service/rest";
-
+    private static final String BASE_URL = "http://api.visitkorea.or.kr/openapi/service/rest";
     private static final String SERVICE_KEY = "A%2BycgFhk2eYE6mEw%2B6%2FhcCbRDaCPGJf3aLCdYyfzuqRx6iY2b%2F04BmXgnQoTrGhm1FBQ%2BOVA5mbMogKlHFcDgw%3D%3D";
 
-    private static String joinParams(String del, String... str) {
+    private static String joinParams(String... str) {
         StringBuilder builder = new StringBuilder(str[0]);
         for (int i = 1; i < str.length; i ++) {
-            builder.append(del).append(str[i]);
+            builder.append("&").append(str[i]);
         }
         return builder.toString();
     }
@@ -49,34 +48,27 @@ public class Main {
         Future<List<Item>> withTourFuture = service.submit(getWithTourServiceCallable());
         Future<List<Item>> greenTourFuture = service.submit(getGreenTourServiceCallable());
 
-        List<Item> jsonResult = new ArrayList<Item>();
+        ArrayList<Item> result = new ArrayList<>();
         try {
             List<Item> korTourList = korFuture.get();
             List<Item> withTourList = withTourFuture.get();
+            List<Item> greenTourList = greenTourFuture.get();
             for (Item item : withTourList) {
-                if (korTourList.contains(item)) {
-                    korTourList.remove(item);
-                }
+                korTourList.remove(item);
             }
 
-            jsonResult.addAll(korTourList);
-            jsonResult.addAll(withTourList);
-            jsonResult.addAll(greenTourFuture.get());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+            result.addAll(korTourList);
+            result.addAll(withTourList);
+            result.addAll(greenTourList);
+        } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
 
-        System.out.println("start " + start());
-        System.out.println("today " + today());
-
-        System.out.println(Arrays.toString(jsonResult.toArray()).replaceAll(",", ",\n"));
-        System.out.println(jsonResult.size());
+        System.out.println(result);
 
         service.shutdown();
         try {
-            if (!service.awaitTermination(1000, TimeUnit.MILLISECONDS)) {
+            if (!service.awaitTermination(500, TimeUnit.MILLISECONDS)) {
                 service.shutdownNow();
             }
         } catch (InterruptedException e) {
@@ -89,8 +81,7 @@ public class Main {
         final long start = start().getTime();
         URL url = null;
         try {
-            url = new URL(END_POINT + "/KorService/areaBasedList?" + joinParams("&",
-                    "ServiceKey=" + SERVICE_KEY,
+            url = new URL(BASE_URL + "/KorService/areaBasedList?" + joinParams("ServiceKey=" + SERVICE_KEY,
                     "MobileOS=ETC",
                     "MobileApp=TEST",
                     "pageNo=1",
@@ -100,10 +91,8 @@ public class Main {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        return createTourAPICallable(url, start, today, new Callback<Item>() {
-            public void on(Item item) {
+        return createTourAPICallable(url, start, today, item -> {
                 // ignore
-            }
         });
     }
 
@@ -112,8 +101,7 @@ public class Main {
         final long start = start().getTime();
         URL url = null;
         try {
-            url = new URL(END_POINT + "/KorWithService/areaBasedList?" + joinParams("&",
-                    "ServiceKey=" + SERVICE_KEY,
+            url = new URL(BASE_URL + "/KorWithService/areaBasedList?" + joinParams("ServiceKey=" + SERVICE_KEY,
                     "MobileOS=ETC",
                     "MobileApp=TEST",
                     "pageNo=1",
@@ -123,11 +111,7 @@ public class Main {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        return createTourAPICallable(url, start, today, new Callback<Item>() {
-            public void on(Item item) {
-                item.setWithTour(true);
-            }
-        });
+        return createTourAPICallable(url, start, today, item -> item.setWithTour(true));
     }
 
     private static Callable<List<Item>> getGreenTourServiceCallable() {
@@ -135,8 +119,7 @@ public class Main {
         final long start = start().getTime();
         URL url = null;
         try {
-            url = new URL(END_POINT + "/GreenTourService/areaBasedList?" + joinParams("&",
-                    "ServiceKey=" + SERVICE_KEY,
+            url = new URL(BASE_URL + "/GreenTourService/areaBasedList?" + joinParams("ServiceKey=" + SERVICE_KEY,
                     "MobileOS=ETC",
                     "MobileApp=TEST",
                     "pageNo=1",
@@ -146,28 +129,29 @@ public class Main {
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
-        return createTourAPICallable(url, start, today, new Callback<Item>() {
-            public void on(Item item) {
-                item.setGreenTour(true);
-            }
-        });
+        return createTourAPICallable(url, start, today, item -> item.setGreenTour(true));
     }
 
     private static Callable<List<Item>> createTourAPICallable(final URL url, final long start, final long end, final Callback<Item> callback) {
-        return new Callable<List<Item>>() {
-            public List<Item> call() throws Exception {
-                ObjectMapper mapper = new ObjectMapper();
-                JsonNode list = mapper.readTree(url).get("response").get("body").get("items").get("item");
-                List<Item> result = mapper.readValue(list.toString(), new TypeReference<List<Item>>(){});
-                ArrayList<Item> filteredList = new ArrayList<Item>();
-                for (Item item : result) {
-                    if (item.getModifiedDate() >= start && item.getModifiedDate() < end) {
-                        callback.on(item);
-                        filteredList.add(item);
-                    }
+        return () -> {
+            // TODO: 리스트 요청을 더 할지 말지에 대한 판단을 하는 로직이 필요.
+            //       1. 요청을 더하기위해서는 URL 이 변해야 하므로 외부에서 URL 빌더를 주입하자.
+            //       2. 결과 즉, 전체 리스트 아이템의 개수와 현재 페이지 정보를 URL빌더에 넘겨주어 다음 URL이 있는 지 확인이 필요하다.
+            //       3. 정의된 기간을 벗어나는 경우의 아이템이 있다면 더이상 요청을 하지 않도록 하고 리턴하자~~!
+            boolean noNeedToFetchMore = true;
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode list = mapper.readTree(url).get("response").get("body").get("items").get("item");
+            List<Item> result = mapper.readValue(list.toString(), new TypeReference<List<Item>>(){});
+            ArrayList<Item> filteredList = new ArrayList<>();
+            for (Item item : result) {
+                if (item.getModifiedDate() >= start && item.getModifiedDate() < end) {
+                    callback.on(item);
+                    filteredList.add(item);
+                } else {
+                    noNeedToFetchMore = false;
                 }
-                return filteredList;
             }
+            return filteredList;
         };
     }
 
